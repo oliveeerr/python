dc9b8a59e8d48cea7121c5bb4768ea4b3d7b18be
Sergi Oliver <93318707+oliveeerr@users.noreply.github.com>
2021-12-17 09:26:13 +0100

    Add files via upload

---
+++ Exercici11.py
@@ -0,0 +1,11 @@
+print("3" == "3")
+print("3" == "3")
+print(3 < 4)
+print(3 < 10)
+print("3" < "4")
+print("3" < "10")
+print( (2 == 2) == "True" )
+print( (2 == 2) == True )
+print(3 < "3")
+
+#Aquest programa el que fara sira dirnos si aquestes afirmacion són vertaderes o falses.
