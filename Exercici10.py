68bcc4514821f0f127dc8b5355b100614633e164
Sergi Oliver <93318707+oliveeerr@users.noreply.github.com>
2021-12-17 09:23:00 +0100

    Add files via upload

---
+++ Exercici10.py
@@ -0,0 +1,17 @@
+x = 5
+y = 10
+z = 10
+print("x < y")
+print("y > z")
+print("x == 5")
+print(not "x == 5")
+print("x != 5")
+print(not "x != 5")
+print("x == 5")
+print("5 == x + 0.00000000001")
+print("x == 5 and y == 10")
+print("x == 5 and y == 5")
+print("x == 5" or "y == 5")
+print(not "x == 5")
+
+#Aquest programa el que fara sira dir si aquestes comparacions son vertaderes o falses però en aquest cas que y=10 z=10 i x=5
