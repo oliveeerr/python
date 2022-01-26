89187d4e5a2485ee7b73faaa5b380f7fc9b261ef
Sergi Oliver <93318707+oliveeerr@users.noreply.github.com>
2022-01-26 10:04:47 +0100

    Delete Exercici4.py

--- Exercici4.py
+++
@@ -1,5 +0,0 @@
-entrada_usuario = (input("Una cereza es A o B:"))
-if entrada_usuario.upper() == "A":
-    print("Correcto, la guinda del postre!")
-else:
-    print("No es correcto, la guinda del desierto es incorrecto")
